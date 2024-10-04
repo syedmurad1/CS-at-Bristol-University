a200cbd9a5b71e59edbde5b5e4e9c6c1db38c6c0
syed <mymurad@gmail.com>
2024-10-05 00:04:35 +0100

    commit

---
+++ Introduction to Python/variable.py
@@ -0,0 +1,8 @@
+name = "Tom"
+age = "90"
+
+print("There once was a human named " + name)
+print("He was " + age + " years old")
+print("He really liked the name " + name)
+print("But didn't like being " + age)
+print(name+" wanted to build time a machine")
