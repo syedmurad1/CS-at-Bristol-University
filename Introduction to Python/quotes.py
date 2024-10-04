e4dbec836780407c8f3be877f90756fc6974a1f9
syed <mymurad@gmail.com>
2024-10-04 21:56:47 +0100

    commit

---
+++ Introduction to Python/quotes.py
@@ -0,0 +1,5 @@
+# Double vs Single Quotes
+print('She said "I like Double Quotes"')
+print("But he said 'I like Single Quotes'")
+
+# print("She said "I like Double Quotes But he said 'I like Single Quotes'")
