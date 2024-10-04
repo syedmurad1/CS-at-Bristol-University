f046197e53377c1c6acb7ad2f4957fe7fc237ec8
syed <mymurad@gmail.com>
2024-10-05 00:06:06 +0100

    commit

---
+++ Introduction to Python/tom.py
@@ -0,0 +1,5 @@
+print("There once was a human named Tom")
+print("He was 90 years old")
+print("He really liked the name Tom")
+print("But didn't like being 90")
+print("Tom wanted to build time a machine")
