079162af277d23a7490ab9a3ca0f6ccecf42eed5
syed <mymurad@gmail.com>
2024-10-05 10:05:55 +0100

    commit

--- Introduction to Python/quotes.py
+++ Introduction to Python/quotes.py
@@ -1,6 +1,7 @@
 # Double vs Single Quotes
 print('She said "I like Double Quotes"')
 print("But he said 'I like Single Quotes'")
+# How to use both
 # print("She said "I like Double Quotes But he said 'I like Single Quotes'")
 print("She said \"I like Double Quotes\" But he said \'I like Single Quotes\'")
 print("She said \"I like Double Quotes\" But \nhe said \'I like Single Quotes\'")
