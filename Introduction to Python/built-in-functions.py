80acc2ada21f4bd4f079d61f9041c1a0d061201f
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2024-10-06 01:00:44 +0100

    Add files via upload

---
+++ Introduction to Python/built-in-functions.py
@@ -0,0 +1,13 @@
+# import
+# math
+
+# Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
+
+# Python has also a built-in module called math, which extends the list of mathematical functions. You must import the math module - import math
+
+import math
+
+x = math.pi
+print(x)
+
+
