68aca30ebfb5d3ce0c4bdbfc53a6c85f5993af13
syed <murad6000@yahoo.com>
2024-10-16 23:07:12 +0100

    commit

---
+++ Data Types and Variables/data-type.py
@@ -0,0 +1,20 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+print("--------------------------------------------------------")
+
+num =6
+print(num)
+print(str(num)+ " is my num")
+# print(num + "my num") 
+# print(num + "my num") - will not work 
---
+++ Data Types and Variables/output_variables.py
@@ -0,0 +1,20 @@
+
+x = "Tom is"
+y = "only"
+z = "99"
+
+a = 100
+b = 200
+# output multiple variables, separated by a comma. Also use the +
+print(x, y, z)
+print(x + y + z) # Notice the space
+
+# For numbers, the + character works as a mathematical operator
+
+print (a+b)
+print (a,b) # output multiple numbers
+
+# to combine a string and a number with the + operator, Python will give an error
+
+# print (x+a) # try
+
