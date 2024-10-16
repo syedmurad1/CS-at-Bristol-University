6951a8b5aa1f3da8e246f2e4598cc1831d83c1a2
syed <murad6000@yahoo.com>
2024-10-16 23:27:43 +0100

    commit

---
+++ Data Types and Variables/global-variables.py
@@ -0,0 +1,48 @@
+
+# Global variables can be used by everyone, both inside of functions and outside
+x = "my love"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+print("--------------------------------------------------------")
+# a variable with the same name inside a function, this variable will be local, and can only be used inside the function
+
+y = "oop"
+
+def myfunc():
+  y = "my firt"
+  print("Python is " + y)
+
+myfunc()
+
+print("Python is " + y)
+
+print("--------------------------------------------------------")
+# The global Keyword
+
+def myfunc():
+  global z
+  z = "fantastic"
+
+myfunc()
+
+print("Python is " + z)
+
+
+print("--------------------------------------------------------")
+#  to change a global variable inside a function
+t = " also a snake"
+
+def myfunc():
+  global t
+  t = "Easy And Fun"
+
+myfunc()
+
+print("Python is " + t)
+
+
+print("--------------------------------------------------------")
