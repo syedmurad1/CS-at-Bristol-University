f66e294a3dcc1d655e16c95940d6cf5b04b48a5a
syed <murad6000@yahoo.com>
2024-10-20 18:12:36 +0100

    commit

---
+++ Data Types and Variables/list_tuple_dictionary.py
@@ -0,0 +1,18 @@
+list = [1, 5, 7, 9, 3, 33] 
+
+tuple = (1, 5, 7, 9, 3, 33)
+
+dictionary_student = {"ID": "1", "name": "Tom","age": 22, "dep": "Computer Science"}
+
+print(dictionary_student)
+# print(sdictionary_student["ID"])
+
+staff = {"John":"manager",
+         "Ali":"supervisor",
+         "Tom":"CEO"}
+
+print(staff)
+print(staff.get("Tom"))
+
+# print(dir(staff))
+# print(help(staff))
---
+++ Data Types and Variables/mutable_immutable.py
@@ -0,0 +1,17 @@
+# mutable-list,dictionary,set,classes...
+
+# immutable - int,float, string, tuple
+
+
+num_1 = [1,2,3] # list
+num_2 = (3,4,5) #tuple
+
+print(num_1[0])
+print(num_2[0])
+
+num_1[0] =100
+print(num_1[0])
+
+# #TypeError: 'tuple' object does not support item assignment
+# num_2[0] =200 
+# print(num_2[0])
