2ea49eefed20640afca7e8fb57e4c95b328f764d
syed <murad6000@yahoo.com>
2024-11-04 10:53:20 +0000

    commit

---
+++ Conditional Statements/n_if_ex.py
@@ -0,0 +1,19 @@
+# Password and Username Checking
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "admin":
+    if password == "i-am-admin":
+        print("Login successful! Welcome, admin.")
+    elif password == "123":
+        print("Weak password. Please reset your password.")
+    else:
+        print("Incorrect password. Please try again.")
+else:
+    if username == "guest":
+        if password == "guest":
+            print("Login successful! Welcome, guest.")
+        else:
+            print("Incorrect password. Please try again.")
+    else:
+        print("Unknown user. Please try again.")
