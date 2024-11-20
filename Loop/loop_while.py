7d65ae275923caa1fd919f191503788545281573
syed <murad6000@yahoo.com>
2024-11-20 19:01:56 +0000

    coomit

--- Loop/loop_while.py
+++ Loop/loop_while.py
@@ -10,3 +10,17 @@ while index < 5:
   print("Tom")
   index += 1 # index = index + 1
 
+
+
+# index = 1
+# while index < 5:
+#   print("Tom")
+#   index += 1
+# else:
+#   print("index is no longer less than 5")
+
+
+i = 0
+while i < 3 :
+    print(i)
+    i = i + 1
