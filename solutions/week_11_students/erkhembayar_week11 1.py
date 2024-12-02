45e58866362814b1028c1eb85dd1faecb58ff588
syed <murad6000@yahoo.com>
2024-12-02 17:28:05 +0000

    commit

---
+++ solutions/week_11_students/erkhembayar_week11 1.py
@@ -0,0 +1,64 @@
+#1
+
+s = 0
+for e in range(1, 11):
+    s += e
+print(s)
+
+#2
+
+password = "worcestershire sauce"
+while True:
+    ent = str(input("Please enter the password: "))
+    if ent == password:
+        print("Correct password")
+        break
+    else:
+        print("Incorrect password")
+
+#3
+
+sum = 0
+x = int(input("Please enter a number: "))
+for i in range(1, x+1):
+    sum += i
+print(sum)
+
+#4
+
+numbers = [12, 75, 100, 150, 180, 145, 525, 50]
+
+for number in numbers:
+    if number >= 500:
+        break
+    if number % 5 != 0:
+        continue
+    if number > 100:
+        continue
+    print(number)
+
+#5
+
+num = 598762
+rev = 0
+while num != 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //= 10
+print(str(rev))
+
+#6
+#while loop
+s = 5
+while s >= 1:
+    print("*"*s)
+    s -= 1
+#for loop
+for c in range (5, 0, -1):
+    print("*"*c)
+
+#7
+
+mult = int(input("Please enter a number: "))
+for j in range(1, 16):
+   print(mult, "x", j, "=", mult*j)
