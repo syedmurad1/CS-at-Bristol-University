ba2229e3136ec4eafbe37a08f84e4434d977a849
syed <murad6000@yahoo.com>
2024-12-13 01:10:05 +0000

    commit

--- File-handling/open.py
+++ File-handling/open.py
@@ -1,3 +1,14 @@
-f = open("C:\Users\syedm\Desktop\Github\CS-at-Bristol-University\File-handling\file1.txt", "r")
-print(f.read())
+file = open(r"C:\Users\syedm\Desktop\Github\CS-at-Bristol-University\File-handling\file1.txt", "r")
+print(file.read())
 
+# t = open(r"C:\Users\syedm\Desktop\Github\CS-at-Bristol-University\File-handling\file1.txt", "r")       #(r"syed.txt", "rt")     
+# print(t.read())
+
+file.close()  # Close the file
+
+
+
+file = open("file1.txt", "r")  # Open the file in read mode
+content = file.read()  # Read content
+print(content)  # Print content
+file.close()  # Close the file
---
+++ File-handling/read.py
@@ -0,0 +1,10 @@
+file = open(r"C:\Users\syedm\Desktop\Github\CS-at-Bristol-University\File-handling\syed.txt", "r")
+# print(file.read())
+
+# print(file.readline(3))  # the first line of the file
+
+print(file.readlines())
+
+file.close()  # Close the file
+
+
