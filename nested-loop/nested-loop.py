c1fb4a6d27fe81f954564293df24fda3fda90852
syed <murad6000@yahoo.com>
2025-01-20 09:52:12 +0000

    commit

---
+++ nested-loop/nested-loop.py
@@ -0,0 +1,25 @@
+for j in range(1, 4):   # Inner loop
+    print(j, end=" ")
+
+
+
+# for i in range(3):       # Outer loop
+#     for j in range(1, 4):   # Inner loop
+#         print(j, end=" ")
+#     print()
+
+
+# i = 0
+# while i < 3:  # Outer loop
+#     j = 1
+#     while j < 4:  # Inner loop
+#         print(j, end=" ")
+#         j += 1
+#     i += 1
+#     print()
+
+
+
+
+
+
