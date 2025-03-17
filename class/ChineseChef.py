bd42be8650921ddc57e58f595b863737fe3d2951
syed <murad6000@yahoo.com>
2025-03-17 12:18:46 +0000

    commit

---
+++ class/ChineseChef.py
@@ -0,0 +1,8 @@
+from Chef import Chef 
+
+class ChineseChef(Chef):
+   def make_dimsum(self):
+       print("The chef makes dim sum")
+
+   def make_special_dish(self):
+       print("The chef makes roast duck")
---
+++ class/classChef.py
@@ -0,0 +1,12 @@
+from Chef import Chef # import Chef class
+from ChineseChef import ChineseChef # import ChineseChef class
+
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
+
+print("............................")
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken()
+myChineseChef.make_special_dish()
